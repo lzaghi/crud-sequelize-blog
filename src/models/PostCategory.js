7ac1d6445ae83ef03d2c3e6ef41ed86be035a411
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-03-06 13:01:48 -0300

    (req11) implementa model PostCategory e associations n:n

---
+++ src/models/PostCategory.js
@@ -0,0 +1,41 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+
+const PostCategoryModel = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+  {
+    postId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    }
+  },
+  {
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    });
+  }
+
+  return PostCategory;
+};
+
+module.exports = PostCategoryModel;
